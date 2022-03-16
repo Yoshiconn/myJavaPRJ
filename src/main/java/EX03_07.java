4d5d9e52139e962f0417cf1bc55872e8fd8a23e6
Yoshiconn <yoshicon0630@gmail.com>
2022-03-16 11:12:46 +0900

    220316 공부내용

---
+++ src/main/java/EX03_07.java
@@ -0,0 +1,12 @@
+public class EX03_07 {
+    public static void main(String[] args) {
+        int a; // 정수형 변수 a를 선언
+        float b; // 실수형 변수 b를 선언
+
+        a = (int) 123.45f; // 정수형 변수 a에 실수를 대입했으며 (int)를 사용해 정수로 변환했다.
+        b = 200; // 실수형 변수 b에 정수 를 대입했다.
+
+        System.out.printf("a의 값 ==> %d \n", a);
+        System.out.printf("b의 값 ==> %f \n", b);
+    }
+}
---
+++ src/main/java/Exam01.java
@@ -0,0 +1,10 @@
+public class Exam01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("양주홍");
+        System.out.println("Yoshicon0630@gmail.com");
+        System.out.println("1999/06/30");
+        System.out.println("화성시 거주중");
+    }
+}
