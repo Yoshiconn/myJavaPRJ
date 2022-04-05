0f4bf54833086ac70177c40bcdd529b3e6744df5
Yoshiconn <yoshicon0630@gmail.com>
2022-04-05 17:01:56 +0900

    220405 코드업 문제풀기

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,26 +4,19 @@ public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
         int i, j;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        for (i = 0; i < a; i++) {
-            System.out.print("*");
-        }
-        System.out.println();
-        for (i = 1; i < a - 1; i++) {
-            for (j = 0; j < a; j++)
-
-                if (j == 0 || j==i || j==a-1-i || j == a-1 || a/2 == i || a/2 == j) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+        for (i = 1; i <= a; i++) {
+            for (j = 1; j <= b; j++) {
+                for (j = 1; j <= b; j++) {
+                    System.out.printf("%d %d \n", i, j);
                 }
-            System.out.println();
+            }
+
         }
-        for (i = 0; i < a; i++) {
-            System.out.print("*");
-        }
+
     }
 }
 
