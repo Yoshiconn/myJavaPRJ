71562c0ffc98805f657e52a709df93af9dd7526a
Yoshiconn <yoshicon0630@gmail.com>
2022-04-19 00:01:01 +0900

    2204019 별찍기

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -1,9 +1,9 @@
 public class Note {
     public static void main(String[] args) {
             int i, j, k;
-            k = 3;
-            for (i = 1; i <= 5; i++) {//1~5까지
-                for (j = 1; j <= 5; j++) {
+            k = 1;
+            for (i = 0; i <= 5; i++) {//1~5까지
+                for (j = 1; j < 6; j++) {
                     if (j < k) {
                         System.out.print("*");
                     } else {
