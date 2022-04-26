7642b039c6f8cc168dc2a088674fa1f2d3fa01d7
Yoshiconn <yoshicon0630@gmail.com>
2022-04-26 10:42:57 +0900

    2204026 자바 연습

--- src/main/java/butterfly1.java
+++ src/main/java/butterfly1.java
@@ -8,16 +8,6 @@ public class butterfly1 {
         int n;
         n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < 2 * n + 1; j++) {
-                if ((j <= 1 + i && j >= 1) || (j >= 2 * n - i && j <= 2 * n)) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
         for (int i = n - 1; i > 0; i--) {
             for (int j = 0; j < 2 * n; j++) {
                 if ((j < (0 + i) && j >= 0) || (j >= (2 * n - i) && j <= 2 * n)) {
