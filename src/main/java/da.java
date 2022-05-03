a5887fa339a2b9f8bc7367fd50d6d2d8166f159c
Yoshiconn <yoshicon0630@gmail.com>
2022-05-03 09:01:35 +0900

    220503 while문 공부

--- src/main/java/da.java
+++ src/main/java/da.java
@@ -5,11 +5,14 @@ public class da {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int hap = 0;
+        int result = 0;
         int i = 1;
         while (i < 10) {
             hap = a * i;
             System.out.printf("%d * %d = %d \n", a, i, hap);
             i++;
+            result += hap;
         }
+        System.out.printf("%d단 의 모든 곱의 합은 %d 입니다.", i,result);
     }
 }
