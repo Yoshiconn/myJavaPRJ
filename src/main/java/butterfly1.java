ca05ceebaf84a60db3995acf3f71d52535fbc915
Yoshiconn <yoshicon0630@gmail.com>
2022-05-17 10:21:05 +0900

    220516 배열공부

--- src/main/java/butterfly1.java
+++ src/main/java/butterfly1.java
@@ -11,9 +11,9 @@ public class butterfly1 {
         for (int i = n - 1; i > 0; i--) {
             for (int j = 0; j < 2 * n; j++) {
                 if ((j < (0 + i) && j >= 0) || (j >= (2 * n - i) && j <= 2 * n)) {
-                    System.out.print("*");
-                } else {
                     System.out.print(" ");
+                } else {
+                    System.out.print("*");
                 }
             }
             System.out.println();
--- src/main/java/ex_05_17.java
+++ src/main/java/ex_05_17.java
@@ -1,17 +1,16 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class arr6 {
+public class ex_05_17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int[] arr = new int[8];
+        int[] arr = new int[9];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        System.out.println(arr[7]);
+        System.out.println(arr[8]);
         int maxNum = arr[arr.length-1];
         System.out.println("--------------------------");
         for(int i : arr) {
