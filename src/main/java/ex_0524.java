574eb6abb071d9ff733e79cc545092a7c1f134de
Yoshiconn <yoshicon0630@gmail.com>
2022-05-24 11:25:27 +0900

    220523 코드업 문제 풀기

---
+++ src/main/java/ex_0524.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex_0524 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[11];
+
+        for(int i = 1; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int num = sc.nextInt();
+        System.out.println(arr[num]);
+    }
+}
