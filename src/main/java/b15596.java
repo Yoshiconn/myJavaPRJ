d48b700c5b4ff09258d801c7285e00f088115237
Yoshiconn <yoshicon0630@gmail.com>
2022-06-10 11:03:08 +0900

    2206010 백준 문제 풀기

---
+++ src/main/java/b15596.java
@@ -0,0 +1,16 @@
+public class b15596 {
+    public static void main(String[] args) {
+
+    }
+    public class Test {
+        long sum(int[] a) {
+            long sum = 0;
+
+            for (int i = 0; i < a.length; i++) {
+                sum += a[i];
+            }
+            return sum;
+        }
+    }
+}
+
--- src/main/java/ex06_09.java
+++ src/main/java/ex06_09.java
@@ -23,4 +23,3 @@ public class ex06_09 {
 
     }
 }
-
